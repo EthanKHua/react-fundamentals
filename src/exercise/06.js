fd18a74b46bf37d4505b0abb0ab70aa37942d1bb
huaethan <ethanhua7378@gmail.com>
2025-10-18 18:01:15 -0400

    completed 07

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -11,10 +11,17 @@ function UsernameForm({onSubmitUsername}) {
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
   const ref = React.createRef()
 
+  const [state, setState] = React.useState('')
+
   function handleSubmit(event) {
     event.preventDefault()
     const username = ref.current.value
     onSubmitUsername(username)
+  }
+
+  function handleInput(event) {
+    event.preventDefault()
+    setState(ref.current.value.toLowerCase())
   }
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
@@ -25,11 +32,12 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='input'>Username:</label>
-        <input type="text" id='input' ref={ref}/>
+        <input type="text" id='input' ref={ref} onInput={handleInput} value={state}/>
       </div>
       <button type="submit">Submit</button>
     </form>
